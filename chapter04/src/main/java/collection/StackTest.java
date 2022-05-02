329866d1653cc192ed95608cad58b9b852e5a7d5
DESKTOP-C5Q1UG1\청출어람_PC02 <mmm01230@gmail.com>
2022-05-02 15:22:27 +0900

    Collect FW#2

---
+++ chapter04/src/main/java/collection/StackTest.java
@@ -0,0 +1,26 @@
+package collection;
+
+import java.util.Stack;
+
+public class StackTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack <String> s = new Stack<>();
+		s.push("둘리");
+		s.push("마이콜");
+		s.push("또치");
+		
+		while(!s.isEmpty()) {
+			String str = s.pop();
+			System.out.println(str);
+		}
+		//비어있는 경우   Excption예외 발생
+		//s.pop();
+		s.push("둘리");
+		s.push("마이콜");
+		s.push("또치");
+		System.out.println(s.pop()+s.peek()+"\n"+s.pop());
+	}
+
+}
