5b362395fd4c03157fb1f04305b8cc50bda496f6
DESKTOP-C5Q1UG1\청출어람_PC02 <mmm01230@gmail.com>
2022-05-03 10:15:35 +0900

    Generic Method & 가변 파라미터 메소드

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,22 +1,37 @@
 package prob01;
 
 public class Printer {
-	public void println(int data) {
-		System.out.println(data);
+//	public void println(int data) {
+//		System.out.println(data);
+//	}
+//
+//	public void println(boolean data) {
+//		System.out.println(data);
+//	}
+//
+//	public void println(double data) {
+//		System.out.println(data);
+//	}
+//
+//	public void println(String data) {
+//		System.out.println(data);
+//	}
+//	public <T> void println(T t) {
+//		System.out.println(t);
+//	}
+	public <T> void println(T... ts) {
+		for (T t : ts) {
+			System.out.println(t+" ");
+		}
+		System.out.println(" ");
 	}
 
-	public void println(boolean data) {
-		System.out.println(data);
+	public int sum(int... nums) {
+		int sum = 0;
+		for (int n : nums) {
+			sum += n;
 
-	}
-
-	public void println(double data) {
-		System.out.println(data);
-
-	}
-
-	public void println(String data) {
-		System.out.println(data);
-
+		}
+		return sum;
 	}
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -5,9 +5,20 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();//각자의 매개변수들로 출력을 할수있게끔
 
-		printer.println( 10 );
-		printer.println( true );
-		printer.println( 5.7 );
-		printer.println( "홍길동" );
+//		printer.println( 10 );
+//		printer.println( true );
+//		printer.println( 5.7 );
+//		printer.println( "홍길동" );
+		
+		printer.println(10);
+		printer.println(true, 5.7);
+		printer.println(5.7,"홍길동",20);
+		printer.println("=====================");
+		
+		System.out.println(printer.sum(1,2));
+		System.out.println(printer.sum(1,2,3,4,5));
+		System.out.println(printer.sum(1,2,3,4,5,6,7,8,9,10));
+
 	}
+	
 }
