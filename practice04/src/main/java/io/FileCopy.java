82b8bf17b61e1d5aed800c9334b97f5971239690
DESKTOP-C5Q1UG1\청출어람_PC02 <mmm01230@gmail.com>
2022-05-03 15:24:48 +0900

    io#4

---
+++ practice04/src/main/java/io/FileCopy.java
@@ -0,0 +1,45 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileCopy {
+
+	public static void main(String[] args) {
+		InputStream is = null;
+		OutputStream os = null;
+
+		try {
+			is = new FileInputStream("aaa.jpg");
+			os = new FileOutputStream("aaa.copy.jpg");
+			int data = -1;
+			while ((data = is.read()) != -1) {
+				os.write(data);
+			}
+		} catch (FileNotFoundException e) {
+
+			System.out.println("file not found:" + e);
+		} catch (IOException e) {
+
+			System.out.println("Error : " + e);
+		} finally {
+			try {
+				if (is != null) {
+					is.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
